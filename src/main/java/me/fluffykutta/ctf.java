8ab0f8d37b01956b09fd3b85ef1a0f1045a63f55
DhruvArora2025 <100000915+DhruvArora2025@users.noreply.github.com>
2022-04-12 14:28:24 -0700

    Commiting all boilerplate code into github.

---
+++ src/main/java/me/fluffykutta/ctf.java
@@ -0,0 +1,17 @@
+package me.fluffykutta;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ctf extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+        
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
